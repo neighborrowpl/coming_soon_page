5ae73a03a417a13e77f7ac6a9b0e68ce67dbc403
mikolajf <mikolaj.fido@gmail.com>
2020-02-02 15:23:32 +0000

    base layout

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,37 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-content>
-      <HelloWorld/>
+      <v-container
+        fluid
+        class="fill-height pa-0 ma-0 justify-end"
+      >
+        <v-row class="fill-height">
+          <v-col
+            cols="12" md="4"
+            class="d-flex align-center mx-auto"
+          >
+            <TextLeft/>
+          </v-col>
+          <v-col
+            md="8"
+            id="right-image"
+            class="d-none d-md-flex"
+          >
+          </v-col>
+        </v-row>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import TextLeft from './components/TextLeft.vue';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    TextLeft,
   },
 
   data: () => ({
@@ -58,3 +39,12 @@ export default {
   }),
 };
 </script>
+
+<style>
+  #right-image {
+    background-image: url('./assets/apartments.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: left bottom;
+  }
+</style>
---
+++ src/components/TextLeft.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <v-row
+      text-left
+      wrap
+    >
+      <v-col cols="10" lg="8" class="mx-auto">
+        <div :class="{'display-1': $vuetify.breakpoint. mdAndDown, 'display-3': $vuetify.breakpoint. mdAndUp}">
+          Neighborrow
+        </div>
+        <div class="text--primary font-italic font-weight-light	mt-6">
+          <p>
+            Wiertarka, sokowirówka, transporter dla kota, namiot gospodarczy, powiększalnik, odkurzacz próżniowy.
+          </p>
+          <p>
+            Tworzymy sąsiedzką wypożyczalnię wszelkich sprzętów w Warszawie. Posiadasz zestaw do warzenia piwa, który wykorzystujesz raz do roku? Potrzebujesz pożyczyć kuchenkę turystyczną na wyjazd w Bieszczady? Neighborrow powstaje aby połączyć posiadających z potrzebującymi mieszkającymi w jednym mieście, po sąsiedzku.
+          </p>
+          <p>
+            Masz pytania, chciałabyś/chciałbyś się z nami podzielić swoimi przemyśleniami? Prosimy o kontakt na <a href="mailto:czesc@neighborrow.pl">czesc@neighborrow.pl</a>.
+          </p>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'TextLeft',
+
+  data: () => ({
+  }),
+};
+</script>
